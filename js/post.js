2e1a7aa67febf1876e8f57be0f443d47115f7b3a
Security Force <webmaster@securityforce.com.do>
2012-12-14 11:17:10 -0400

    Version 1.0.1
    Ahora para subir y publicar

---
+++ js/post.js
@@ -0,0 +1,27 @@
+// con deviceready nos aseguramos que el dispositivo este listo para ser usado
+$(document).bind('deviceready', function(){
+    $(function(){
+        $('form').submit(function(){
+            var dataID = $(this).parent().attr('data-datos-id');
+            var postData = $(this).serialize();
+            $.ajax({
+                type: 'POST',
+                data: postData+'&lid='+dataID,
+                // cargamos la url del servidor externo
+                url: 'http://securityforce.com.do/test/guardar.php',
+                success: function(data){
+                    console.log(data);
+                    $('#email').val('');
+                    $('#comentario').val('');
+                //    alert('Tu comentario fue agregado');
+					navigator.notification.alert('Tu comentario fue agregado',null,'Mi aplicacion', 'Aceptar');
+                },
+                error: function(data){
+                    console.log(data);
+                    alert('Ocurrio un error al enviar tu comentario');
+                }
+            });
+            return false;
+        });
+    });
+});
